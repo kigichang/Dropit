db7ddc34e95d3c45d297fcba1a4409a37ce80233
kigi <kigi.chang@gmail.com>
2014-08-14 15:14:29 +0800

    final version

---
+++ DropIt/DropitBehavior.swift
@@ -0,0 +1,46 @@
+//
+//  DropitBehavior.swift
+//  DropIt
+//
+//  Created by kigi on 8/14/14.
+//  Copyright (c) 2014 classroomM. All rights reserved.
+//
+
+import UIKit
+
+class DropitBehavior: UIDynamicBehavior {
+
+// MARK: Properties
+
+    lazy var gravity: UIGravityBehavior = { () -> UIGravityBehavior in
+        var g = UIGravityBehavior()
+        g.magnitude = 0.9
+        return g
+    }()
+
+
+    lazy var collider: UICollisionBehavior = { () -> UICollisionBehavior in
+        var c = UICollisionBehavior()
+        c.translatesReferenceBoundsIntoBoundary = true
+        return c
+    }()
+    
+
+    override init() {
+        super.init()
+        self.addChildBehavior(self.gravity)
+        self.addChildBehavior(self.collider)
+    }
+
+    // TODO:
+    func addItem(item: UIDynamicItem) {
+        self.gravity.addItem(item)
+        self.collider.addItem(item)
+    }
+    
+    // TODO:
+    func removeItem(item: UIDynamicItem) {
+        self.gravity.removeItem(item)
+        self.collider.removeItem(item)
+    }
+}
--- DropIt/ViewController.swift
+++ DropIt/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 let GLOBAL_DROP_SIZE = CGSize(width: 40, height: 40)
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDynamicAnimatorDelegate {
     
 // MARK: Properties
     class var DROP_SIZE : CGSize {
@@ -20,24 +20,80 @@ class ViewController: UIViewController {
     @IBOutlet var gameView: UIView!
     
     lazy var animator: UIDynamicAnimator = { () -> UIDynamicAnimator in
-        return UIDynamicAnimator(referenceView: self.gameView!)
+        var a = UIDynamicAnimator(referenceView: self.gameView!)
+        a.delegate = self
+        return a
     }()
 
-    lazy var gravity: UIGravityBehavior = { () -> UIGravityBehavior in
-        var g = UIGravityBehavior()
-        g.magnitude = 0.9
-        self.animator.addBehavior(g)
-        return g
-    }()
-
-
-    lazy var collider: UICollisionBehavior = { () -> UICollisionBehavior in
-        var c = UICollisionBehavior()
-        c.translatesReferenceBoundsIntoBoundary = true
-        self.animator.addBehavior(c)
-        return c
+    lazy var dropitBehavior: DropitBehavior = { () -> DropitBehavior in
+        var d = DropitBehavior()
+        self.animator.addBehavior(d)
+        return d
     }()
     
+// MARK: UIDynamicAnimatorDelegate
+
+    /* 會需要等到整個動畫停止了，才會被 trigger */
+    func dynamicAnimatorDidPause(animator: UIDynamicAnimator) {
+        self.removeCompletedRows()
+    }
+
+    func removeCompletedRows() -> Bool {
+        
+        var dropsToRemove = [UIView]()
+        
+        for (var y = self.gameView.bounds.size.height - ViewController.DROP_SIZE.height / CGFloat(2); y > 0; y -= ViewController.DROP_SIZE.height) {
+            var rowIsComplete = true
+            var dropsFound = [UIView]()
+
+            for (var x  = ViewController.DROP_SIZE.width / CGFloat(2); x <= self.gameView.bounds.size.width - ViewController.DROP_SIZE.width / CGFloat(2); x += ViewController.DROP_SIZE.width) {
+                if let hit = self.gameView.hitTest(CGPointMake(x, y), withEvent: nil) {
+                    if hit.superview == self.gameView {
+                        dropsFound.append(hit)
+                    }
+                    else {
+                        rowIsComplete = false
+                        break;
+                    }
+                }
+            }
+            
+            if dropsFound.count == 0 {
+                break
+            }
+
+            if rowIsComplete {
+                dropsToRemove += dropsFound
+            }
+        }
+        
+        if dropsToRemove.count > 0 {
+            for drop in dropsToRemove {
+                self.dropitBehavior.removeItem(drop)
+            }
+            self.animateRevovingDrops(dropsToRemove)
+        }
+
+        return false
+    }
+
+    func animateRevovingDrops(dropsToRemove: [UIView]) {
+        UIView.animateWithDuration(1.0,
+            animations: {() in
+                for drop in dropsToRemove {
+                    let x = (Int(arc4random()) % (Int(self.gameView.bounds.size.width) * 5)) - Int(self.gameView.bounds.size.width) * 2
+                    
+                    let y = self.gameView.bounds.size.height;
+                    drop.center = CGPointMake(CGFloat(x), -y);
+                }
+            },
+            completion: { (finished: Bool) in
+                for drop in dropsToRemove {
+                    drop.removeFromSuperview()
+                }
+            })
+    }
+
 // MARK: Gesture Action
     
     @IBAction func tap(sender: UITapGestureRecognizer) {
@@ -47,14 +103,15 @@ class ViewController: UIViewController {
     
     func drop() {
         var frame = CGRect(origin: CGPointZero, size: ViewController.DROP_SIZE)
-        frame.origin.x = ((CGFloat(arc4random()) % self.gameView.bounds.size.width) / ViewController.DROP_SIZE.width) * ViewController.DROP_SIZE.width
+        let x = Int(arc4random()) % Int(self.gameView.bounds.size.width) / Int(ViewController.DROP_SIZE.width)
+
+        frame.origin.x = CGFloat(x) * ViewController.DROP_SIZE.width
         
         var dropView = UIView(frame: frame)
         dropView.backgroundColor = self.randomColor()
         
         self.gameView.addSubview(dropView)
-        self.gravity.addItem(dropView)
-        self.collider.addItem(dropView)
+        self.dropitBehavior.addItem(dropView)
     }
     
     

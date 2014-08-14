daf6c3d0df539cf1e7859c32b99eb9138a481a80
kigi <kigi.chang@gmail.com>
2014-08-14 11:29:49 +0800

    Initial Commit

---
+++ DropIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropIt
+//
+//  Created by kigi on 8/14/14.
+//  Copyright (c) 2014 classroomM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
